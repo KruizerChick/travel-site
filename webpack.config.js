c1da7a2f09ca4d14437e260de5649856b1c4a276
KruizerChick <kruizerchick@gmail.com>
2017-05-25 12:29:57 -0500

    Completed lazy loading of images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    // Define the base JS file
-    entry: "./app/assets/scripts/App.js",
+    // Define the base JS file(s)
+    entry: {
+      App: "./app/assets/scripts/App.js",
+      Vendor: "./app/assets/scripts/Vendor.js"
+    },
     // Define the bundled JS file and file path
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
-        filename: "App.js"
+        filename: "[name].js"
     },
     // Use Babel to convert JS files from ES2015 (excluding any in /node_modules)
     module: {
